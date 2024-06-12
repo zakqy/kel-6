70f560e53f3f0b334d60e6a11f06f3a713d9e727
zakqy <154299040+zakqy@users.noreply.github.com>
2024-06-12 08:04:54 +0700

    Delete lib directory

--- lib/config/asset.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter/material.dart';
-
-class Asset{
-  static Color colorPrimaryDark = Color.fromARGB(255, 2, 84, 100);
-  static Color colorPrimary = Color.fromRGBO(69, 143, 4, 1);
-  static Color colorSecondary = Color.fromARGB(255, 143, 13, 4);
-  static Color colorAccent = Color.fromARGB(255, 143, 4, 4);
-}
